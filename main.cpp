0fc5661b857c14bb307b513fc0f2287eab727283
Jan Halsema <janhlsm@gmail.com>
2020-01-30 13:40:15 +0100

    it blink now?

--- main.cpp
+++ main.cpp
@@ -1,25 +1,33 @@
 #include <stdint.h>
+
 extern "C" {
-#include <gd32vf103_gpio.h>
-#include <gd32vf103_rcu.h>
+    #include <gd32vf103.h>
+    #include <gd32vf103_gpio.h>
 }
 
 void nop_loop(int nops){
-  for(int i = 0; i < nops; i++){
-    __asm("nop");
-  }
+    for(int i = 0; i < nops; i++){
+        __asm("nop");
+    }
 }
 
 constexpr uint32_t GPIO_P = GPIOC;
-constexpr uint32_t PIN = 13;
+constexpr uint32_t PIN = 1U << 13;
 
-int main(){
-  rcu_periph_clock_enable(RCU_GPIOA);
-  gpio_init(GPIO_P,GPIO_MODE_OUT_PP,GPIO_OSPEED_50MHZ,PIN);
-  bool state = false;
-  while(true){
-    gpio_bit_write(GPIO_P, PIN, (bit_status) state);
-    state = !state;
-    nop_loop(100);
-  }
+
+
+int main() {
+
+    // SystemInit();
+
+
+    rcu_periph_clock_enable(RCU_GPIOC);
+    gpio_init(GPIO_P, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, PIN);
+    
+    while (true) {
+        GPIO_BOP(GPIO_P) = PIN;
+        nop_loop(18'000'000);
+        GPIO_BC(GPIO_P) = PIN;
+        nop_loop(18'000'000);
+    }
 }
